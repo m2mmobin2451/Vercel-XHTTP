a185abf870329919a0a1d2d9ef84f503c25a2959
m2mmobin2451 <m2mmobinafrinzad@gmail.com>
2026-04-29 01:07:34 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 export const config = { runtime: "edge" };
 
-const TARGET_BASE = (process.env.m2m || "").replace(/\/$/, "");
+const TARGET_BASE = (process.env.TARGET_DOMAIN || "").replace(/\/$/, "");
 
 const STRIP_HEADERS = new Set([
   "host",
